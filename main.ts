4dbc69e76f858104d533ef166b2dc19cd496aadc
imamiya-masaki <imamiyagit@gmail.com>
2023-09-04 07:13:05 +0900

    init

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 import { sleep } from "https://deno.land/x/sleep/mod.ts";
-import {
-  app,
-  get
-} from "https://denopkg.com/syumai/dinatra/mod.ts";
+// import { oakCors } from "https://deno.land/x/cors/mod.ts";
+// import {
+//   app,
+//   get
+// } from "https://denopkg.com/syumai/dinatra/mod.ts";
 
-app(
-  get("/", () => "helth ok"),
-  get("/:time", async ({ params }) =>{
-    const num = params.time ?? 5 // デフォルト5s
-    await sleep(num)
-    return "ok"
-  }),
-);
+import { serve } from 'https://deno.land/std@0.178.0/http/server.ts'
+
+type Query = {
+  time: number
+}
+
+async function handler(request: Request) {
+  let url = new URL(request.url)
+  const time = url.searchParams.get("time") ?? "5"
+  await sleep(Number(time))
+  return new Response('Hello', {
+  })
+}
+
+serve(handler)
